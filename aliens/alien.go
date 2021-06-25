c85469937a2f8446a1223b2138539e30d9e5c536
Mojtaba <mojtaba-esk@users.noreply.github.com>
2021-06-25 11:59:12 +0200

    update some comments to look nicer on docs

--- aliens/alien.go
+++ aliens/alien.go
@@ -19,10 +19,10 @@ func newAlien(name string) *Alien {
 
 // This function (rather method) start the invasion of the alien
 // It first enters to the given city then finds all available outgoing paths of the city
-//  then it randomly chooses a path and moves to another city
+// then it randomly chooses a path and moves to another city
 // If it gets trapped i.e. there is no way to go out of the city, it terminates itself
 // When an alien enters a city, we need to keep track of all invaders; in order to avoid race condition,
-//  we consider it as a critical section and protect each city with an individual mutex
+// we consider it as a critical section and protect each city with an individual mutex
 func (a *Alien) Invade(currentCity *world.City, wg *sync.WaitGroup, mutexList citiesMutexList) {
 
 	defer wg.Done()
